b414232811743266c6a1b95f8dfe9a8846ad2c3e
Jonathan Chen <sabza178@gmail.com>
2025-03-15 15:04:29 +0700

    "Add all file"

---
+++ CloneProject-F/src/layouts/PageHeader.tsx
@@ -0,0 +1,32 @@
+import { Bell, Menu, Upload, User } from "lucide-react";
+import logo from "../assets/react.svg";
+import { Button } from "../components/Button";
+
+export function PageHeader() {
+  return (
+    <div className="flex gap-10 lg:gap-20 justify-between pt-2 mb-6 mx-4">
+      <div className="flex gap-4 items-center flex-shrink-0">
+        <Button variant="ghost" size="icon" >
+          <Menu />
+        </Button>
+        <a href="/">
+          <img src={logo} className="h-6" />
+        </a>
+      </div>
+      <div></div>
+      <div className="flex flex-shrink-0 md:gap-2">
+        <Button variant="ghost" size="icon">
+          <Upload/>
+        </Button>
+        <Button variant="ghost" size="icon">
+          <Bell/>
+        </Button>
+        <Button variant="ghost" size="icon">
+          <User/>
+        </Button>
+
+      </div>
+    </div>
+  );
+}
+export default PageHeader;
