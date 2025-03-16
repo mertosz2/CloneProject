7d9ae436c3c511e77ec3ad79c966a326b9e3758b
Jonathan Chen <sabza178@gmail.com>
2025-03-16 22:07:15 +0700

    add @theme in css and create input form.

--- CloneProject-F/src/layouts/PageHeader.tsx
+++ CloneProject-F/src/layouts/PageHeader.tsx
@@ -1,4 +1,4 @@
-import { Bell, Menu, Upload, User } from "lucide-react";
+import { Bell, Menu, Search, Upload, User } from "lucide-react";
 import logo from "../assets/react.svg";
 import { Button } from "../components/Button";
 
@@ -13,7 +13,16 @@ export function PageHeader() {
           <img src={logo} className="h-6" />
         </a>
       </div>
-      <div></div>
+      <form className="flex flex-grow justify-center">
+        <div className="flex flex-grow max-w-[600px]">
+        <input className="rounded-l-full border border-secondary-border shadow shadow-secondary" placeholder="Search"  ></input>
+
+        <Button className="rounded-r-full">
+          <Search />
+        </Button>
+        </div>
+
+      </form>
       <div className="flex flex-shrink-0 md:gap-2">
         <Button variant="ghost" size="icon">
           <Upload/>
